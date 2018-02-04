c3c84859eabb607fbf210473c3c3d850e332f01a
jacwilso <j.wilson3355@yahoo.com>
2018-02-03 23:34:58 -0500

    Completed zipping and unzipping, processing json files

--- Assets/_Scripts/JsonParser.cs
+++ Assets/_Scripts/JsonParser.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System.IO;
 
 public class JsonParser : MonoBehaviour
 {
@@ -35,6 +36,9 @@ public class JsonParser : MonoBehaviour
 
     #region JSON_ZIP
 
+    private static string json_ext = "json";
+    private static string zip_ext = "zip";
+
     public void ToJsonZipUI()
     {
         ExampleClass obj = new ExampleClass();
@@ -45,30 +49,53 @@ public class JsonParser : MonoBehaviour
         string json = JsonUtility.ToJson(obj);
 
         string fileName = "ToJsonExample";
-        string extensions = "txt";
-        string path = Crosstales.FB.FileBrowser.SaveFile("Save File", "", fileName, extensions);
-        System.IO.File.WriteAllText(path, json);
+        string filePath = Crosstales.FB.FileBrowser.SaveFile("Save File", "", fileName, json_ext);
+        string folderPath = filePath.Substring(0, filePath.Length - json_ext.Length - 1);
 
-        // SAVE TO FOLDER
-        UniZip.Compress(@"c:\Users\jacobwil\Downloads\ToJsonExample");
-        //UniZip.CompressDirectory(path, "", (file) => { text.text = "Compressing {0}..." + file; });
+        folderPath = UniqueDirectoryName(folderPath);
+        Directory.CreateDirectory(folderPath);
+        File.WriteAllText(System.IO.Path.Combine(folderPath, fileName + "." + json_ext), json);
+
+        string zipPath = folderPath + "." + zip_ext;
+        System.IO.Compression.ZipFile.CreateFromDirectory(folderPath, zipPath);
     }
 
     public void FromJsonZipUI()
     {
-        string extensions = "";
-        string path = Crosstales.FB.FileBrowser.OpenSingleFile("Open File", "", extensions);
-        string dst = path.Substring(0, path.Length - 4);
-        UniZip.DecompressFolder(path);
-        //UniZip.DecompressToDirectory(path, "", (fileName) => { text.text = "Decompressing {0}..." + fileName; });
+        string zipPath = Crosstales.FB.FileBrowser.OpenSingleFile("Open File", "", zip_ext);
+        string extractPath = zipPath.Substring(0, zipPath.Length - zip_ext.Length - 1);
+        extractPath = UniqueDirectoryName(extractPath);
+        System.IO.Compression.ZipFile.ExtractToDirectory(zipPath, extractPath);
+        DirectoryInfo dir = new DirectoryInfo(extractPath);
 
+        text.text = "";
+
+        FileInfo[] files = dir.GetFiles("*." + json_ext);
+        for (int i = 0; i < files.Length; i++)
+        {
+            text.text += FromJsonFile(files[i].FullName);
+        }
+    }
+
+    private string UniqueDirectoryName(string path)
+    {
+        if (!Directory.Exists(path))
+        {
+            return path;
+        }
+
+        int folder_num = 1;
+        while (Directory.Exists(path + " (" + folder_num + ")"))
+        {
+            folder_num++;
+        }
+        return path + " (" + folder_num + ")";
     }
 
     #endregion 
 
     public string FromJsonFile (string path)
 	{
-
 		string json = System.IO.File.ReadAllText(path);
 		ExampleClass obj = JsonUtility.FromJson<ExampleClass>(json);
 
