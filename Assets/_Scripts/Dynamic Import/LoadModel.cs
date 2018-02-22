3da4637336e5ee4cf0de89a1efe6c148f85b9481
Jacob Wilson <jacwilso0@gmail.com>
2018-02-22 15:38:15 -0500

    Changed the texture looking path for TriLib

--- Assets/_Scripts/Dynamic Import/LoadModel.cs
+++ Assets/_Scripts/Dynamic Import/LoadModel.cs
@@ -17,6 +17,7 @@ public class LoadModel : MonoBehaviour {
             { //Initializes our Asset Loader.
                 var assetLoaderOptions = ScriptableObject.CreateInstance<AssetLoaderOptions>(); //Creates an Asset Loader Options object.
                 assetLoaderOptions.AutoPlayAnimations = true; //Indicates that our model will automatically play its first animation, if any.
+                assetLoaderOptions.TexturesPathOverride = Path.GetFullPath("./Assets/Models/Textures");
                 var filename = Path.Combine(Path.GetFullPath("./Assets/Models"), fileName); //Combines our current directory with our model filename "turtle1.b3d" and generates the full model path.
                 assetLoader.LoadFromFile(filename, assetLoaderOptions); //Loads our model.
             }
