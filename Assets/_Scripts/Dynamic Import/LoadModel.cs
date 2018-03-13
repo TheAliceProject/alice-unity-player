c0528aa7ccbd675b1885b88e6191c19566fd2906
jacwilso <j.wilson3355@yahoo.com>
2018-03-13 16:24:25 -0400

    Updated dynamic model loading for specific texture files

--- Assets/_Scripts/Dynamic Import/LoadModel.cs
+++ Assets/_Scripts/Dynamic Import/LoadModel.cs
@@ -1,12 +1,14 @@
 ﻿using UnityEngine;
 using TriLib;
 using System.IO;
+using System;
 
 public class LoadModel : MonoBehaviour {
 
     [SerializeField] private readonly bool useTriLib = true;
     [Tooltip("Can only load models from the Asset/Models folder per this script.")]
     [SerializeField] private string fileName = "alice.dae";
+	[SerializeField] private Texture texOverride;
 
     private void Start()
     {
@@ -17,10 +19,22 @@ public class LoadModel : MonoBehaviour {
             { //Initializes our Asset Loader.
                 var assetLoaderOptions = ScriptableObject.CreateInstance<AssetLoaderOptions>(); //Creates an Asset Loader Options object.
                 assetLoaderOptions.AutoPlayAnimations = true; //Indicates that our model will automatically play its first animation, if any.
-                assetLoaderOptions.TexturesPathOverride = Path.GetFullPath("./Assets/Models/Textures");
-                var filename = Path.Combine(Path.GetFullPath("./Assets/Models"), fileName); //Combines our current directory with our model filename "turtle1.b3d" and generates the full model path.
-                assetLoader.LoadFromFile(filename, assetLoaderOptions); //Loads our model.
-            }
+				if (texOverride)
+				{
+					assetLoader.OnMaterialCreated += MaterialCreated;
+				} else
+				{
+					assetLoaderOptions.TexturesPathOverride = Path.GetFullPath("./Assets/Models/Textures");
+				}
+
+				var filename = Path.Combine(Path.GetFullPath("./Assets/Models"), fileName); //Combines our current directory with our model filename "turtle1.b3d" and generates the full model path.
+				assetLoader.LoadFromFile(filename, assetLoaderOptions); //Loads our model.
+			}
         }
     }
+
+	private void MaterialCreated(uint materialIndex, bool isOverriden, Material material)
+	{
+		material.SetTexture("_MainTex", texOverride);
+	}
 }
