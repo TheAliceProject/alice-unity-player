392c0bf73b43967bfffc96ed763e97cbc5cfa03f
Daniel Abeshouse <dabeshou@andrew.cmu.edu>
2018-06-07 11:25:39 -0400

    Add static method/field access as distinct expressions

---
+++ Assets/_Scripts/Tweedle/Tweedle/Expression/StaticAccessExpression.cs
@@ -0,0 +1,13 @@
+﻿namespace Alice.Tweedle
+{
+    abstract public class StaticAccessExpression : TweedleExpression
+    {
+		protected TweedleTypeDeclaration target;
+
+		public StaticAccessExpression(TweedleTypeDeclaration target)
+            : base(null)
+        {
+            this.target = target;
+        }
+    }
+}
--- Assets/_Scripts/Tweedle/Tweedle/Runtime/TweedleFrame.cs
+++ Assets/_Scripts/Tweedle/Tweedle/Runtime/TweedleFrame.cs
@@ -1,10 +1,131 @@
-﻿namespace Alice.Tweedle
-{
-    public class TweedleFrame
-    {
-        public TweedleFrame(Tweedle.TweedleObject instance)
-        {
+﻿using System;
 
+namespace Alice.Tweedle
+{
+	public class TweedleFrame
+	{
+		protected TweedleFrame parent;
+		private TweedleObject thisValue;
+		//private TweedleObject messageTarget;
+		//private TweedleValue returnValue = TweedleNull.NULL;
+
+		protected Action<TweedleValue> nextStep;
+
+		public TweedleFrame()//Tweedle.TweedleObject instance)
+		{
+
+		}
+
+		internal TweedleFrame ParallelFrame(int count)
+		{
+			return new ParallelFrame(this, count);
+		}
+        
+		protected TweedleFrame(TweedleFrame parent)
+		{
+			this.parent = parent;
+		}
+
+		protected TweedleFrame(TweedleFrame parent, Action<TweedleValue> next)
+        {
+            this.parent = parent;
+			nextStep = next;
         }
-    }
+
+		//internal SequentialFrame SequentialFrame(int count)
+		//{
+		//	return new SequentialFrame(this, count);
+		//}
+
+		public TweedleFrame ExecutionFrame(Action<TweedleValue> next)
+        {
+            return new TweedleFrame(this, next);
+        }
+
+		internal TweedleObject GetThis() {
+			return thisValue;
+		}
+
+		//TweedleObject GetMessageTarget() {
+			//return messageTarget;
+		//}
+
+		//internal TweedleValue GetReturnValue()
+		//{
+		//	return returnValue;
+		//}
+
+		internal void Next(TweedleValue val)
+		{
+			nextStep(val);
+		}
+
+        internal void Next()
+		{
+			nextStep(TweedleNull.NULL);
+        }
+
+		//internal void SetReturnValue(TweedleValue tweedleValue)
+		//{
+		//	throw new NotImplementedException();
+		//}
+
+		internal void SetParameterValue(Type type, TweedleValue tweedleValue)
+		{
+			throw new NotImplementedException();
+		}
+
+		internal TweedleFrame PopMethod()
+		{
+			// Walk parents and return the caller
+			throw new NotImplementedException();
+		}
+
+		internal TweedleFrame MethodCallFrame(TweedleMethod tweedleMethod)
+		{
+            return new TweedleFrame(this);
+		}
+	}
+
+	/*internal class SequentialFrame : TweedleFrame
+	{
+		private int count;
+
+		public SequentialFrame(TweedleFrame frame, int count)
+            : base(frame)
+		{
+			this.count = count;
+		}
+	}*/
+
+	internal class ParallelFrame : TweedleFrame
+	{
+		private int count;
+
+		public ParallelFrame(TweedleFrame frame, int count)
+			:base(frame)
+		{
+			this.count = count;
+			nextStep = ignored =>
+			{
+				this.count--;
+				if (this.count == 0)
+				{
+					parent.Next();
+				}
+			};
+		}
+
+		/*private static Action<TweedleValue> CountDown()
+		{
+			return ignored =>
+			{
+				count--;
+                if (count == 0)
+				{
+					parent.Next(ignored);
+				}
+			};
+		}*/
+	}
 }
