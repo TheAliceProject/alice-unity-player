df5d333739bd2cffc62bf449079ccd278a9d3f23
Dave Culyba <dculyba@andrew.cmu.edu>
2018-06-08 16:36:31 -0400

    Added skin weight normalization and extra test files
    Fixed TriLib's collada weight handling by normalizing the skin weights on load
    Removed texture specification to allow texture references to be loaded properly
    Added extra text models

--- Assets/_Scripts/Dynamic Import/LoadModel.cs
+++ Assets/_Scripts/Dynamic Import/LoadModel.cs
@@ -2,13 +2,77 @@
 using TriLib;
 using System.IO;
 using System;
+using System.Collections;
+using System.Collections.Generic;
 
 public class LoadModel : MonoBehaviour {
 
     [SerializeField] private readonly bool useTriLib = true;
     [Tooltip("Can only load models from the Asset/Models folder per this script.")]
-    [SerializeField] private string fileName = "alice.dae";
+    [SerializeField] private string fileName = "ColaBottle.dae";
 	[SerializeField] private Texture texOverride;
+
+    private List<Mesh> GetMeshes( Transform rootTransform )
+    {
+        List<Mesh> meshes = new List<Mesh>();
+
+        for (int i=0; i<rootTransform.childCount; i++)
+        {
+            Transform child = rootTransform.GetChild(i);
+            SkinnedMeshRenderer skin = child.gameObject.GetComponent<SkinnedMeshRenderer>();
+            if (skin != null)
+            {
+                meshes.Add(skin.sharedMesh);
+            }
+            else
+            {
+                meshes.AddRange(GetMeshes(child));
+            }
+        }
+        return meshes;
+    }
+
+    private void CheckWeights(Mesh mesh)
+    {
+        Debug.Log(mesh.name + "START");
+        Debug.Log("Bone Weights Check:");
+        for (int i = 0; i < mesh.boneWeights.Length; i++)
+        {
+            BoneWeight weight = mesh.boneWeights[i];
+
+            double weightTotal = weight.weight0 + weight.weight1 + weight.weight2 + weight.weight3;
+            if (Math.Abs(1 - weightTotal) > .001)
+            {
+                Debug.Log("  "+i+" BAD WEIGHTS: "+weightTotal);
+            }
+        }
+        Debug.Log(mesh.name + " DONE");
+    }
+
+    private void NormalizeWeightsInMesh(Mesh mesh)
+    {
+        BoneWeight[] newWeights = mesh.boneWeights;
+        for (int i = 0; i < mesh.boneWeights.Length; i++)
+        {
+            BoneWeight weight = newWeights[i];
+            double weightTotal = weight.weight0 + weight.weight1 + weight.weight2 + weight.weight3;
+            weight.weight0 = (float)(weight.weight0 / weightTotal);
+            weight.weight1 = (float)(weight.weight1 / weightTotal);
+            weight.weight2 = (float)(weight.weight2 / weightTotal);
+            weight.weight3 = (float)(weight.weight3 / weightTotal);
+            newWeights[i] = weight;
+        }
+        mesh.boneWeights = newWeights;
+    }
+
+    private void NormalizeWeightsInModel(GameObject model)
+    {
+        List<Mesh> meshes = GetMeshes(model.transform);
+        foreach(Mesh mesh in meshes)
+        {
+            NormalizeWeightsInMesh(mesh);
+        }
+    } 
 
     private void Start()
     {
@@ -18,17 +82,10 @@ public class LoadModel : MonoBehaviour {
             using (var assetLoader = new AssetLoader())
             { //Initializes our Asset Loader.
                 var assetLoaderOptions = ScriptableObject.CreateInstance<AssetLoaderOptions>(); //Creates an Asset Loader Options object.
-                assetLoaderOptions.AutoPlayAnimations = true; //Indicates that our model will automatically play its first animation, if any.
-				if (texOverride)
-				{
-					assetLoader.OnMaterialCreated += MaterialCreated;
-				} else
-				{
-					assetLoaderOptions.TexturesPathOverride = Path.GetFullPath("./Assets/Models/Textures");
-				}
-
+                assetLoaderOptions.AutoPlayAnimations = false;
 				var filename = Path.Combine(Path.GetFullPath("./Assets/Models"), fileName); //Combines our current directory with our model filename "turtle1.b3d" and generates the full model path.
-				assetLoader.LoadFromFile(filename, assetLoaderOptions); //Loads our model.
+				GameObject loadedModel = assetLoader.LoadFromFile(filename, assetLoaderOptions); //Loads our model.
+                NormalizeWeightsInModel(loadedModel);
 			}
         }
     }
