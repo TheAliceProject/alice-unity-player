ae3573df03176b915d3e33f8d6e2a87ddf5844ce
Jonathan Kieffer <jkieffer@filamentgames.com>
2018-10-08 17:35:00 -0500

    - NullTypes can be casted to any ReferenceTypes - Fixes to Tweedle library

--- Assets/_Scripts/Player/Modules/MathModule.cs
+++ Assets/_Scripts/Player/Modules/MathModule.cs
@@ -10,6 +10,13 @@ namespace Alice.Player.Modules
     [PInteropType("Math")]
     static public class MathModule
     {
+        [PInteropField]
+        public const double POSITIVE_INFINITY = double.PositiveInfinity;
+        [PInteropField]
+        public const double NEGATIVE_INFINITY = double.NegativeInfinity;
+        [PInteropField]
+        public const double PI = System.Math.PI;
+
         [PInteropMethod]
         static public double sqrt(double number) {
             return System.Math.Sqrt(number);
