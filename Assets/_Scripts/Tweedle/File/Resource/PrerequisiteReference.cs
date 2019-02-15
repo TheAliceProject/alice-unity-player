a45de9e74a91d45b3d03f88485501ac08e65d22f
Jonathan Kieffer <jkieffer@filamentgames.com>
2019-02-15 12:02:50 -0600

    added prerequisite support for project manifests

---
+++ Assets/_Scripts/Tweedle/File/Resource/PrerequisiteReference.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Alice.Tweedle.File
+{
+    [System.Serializable]
+    public class PrerequisiteReference 
+    {
+        public ProjectIdentifier identifier;
+        public string manifest;
+    }
+}
