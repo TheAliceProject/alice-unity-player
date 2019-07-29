51c5417cfee8772588eff0e967802b772c3cc89f
rfrankfilament <rfrank@filamentgames.com>
2019-07-29 14:59:29 -0500

    Working string input

--- Assets/_Scripts/Player/Modules/DialogModule.cs
+++ Assets/_Scripts/Player/Modules/DialogModule.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics;
 using Alice.Tweedle.Interop;
+using Alice.Player.Unity;
 using System.Collections;
 using BeauRoutine;
 
@@ -8,20 +9,10 @@ namespace Alice.Player.Modules
     [PInteropType("Dialog")]
     static public class DialogModule
     {
-        public static string returnString;
-
         [PInteropMethod]
         public static AsyncReturn<string> getStringFromUser(string message) {
-            UnityEngine.Debug.Log("DialogModule: " + message);
-            AsyncReturn<string> returnString = new AsyncReturn<string>();
-            Routine.Start(GetStringThing(returnString));
-            return returnString;
-        }
-
-        private static IEnumerator GetStringThing(AsyncReturn<string> returnString)
-        {
-            yield return 3f;
-            returnString.Return("m'callback");
+            SceneCanvas canvas = SceneGraph.Current.Scene.GetCurrentCanvas();
+            return canvas.UserInputControl.spawnStringInput(message);
         }
     }
 }
--- Assets/_Scripts/Player/Modules/SceneGraph/UI/UserInput.cs
+++ Assets/_Scripts/Player/Modules/SceneGraph/UI/UserInput.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using Alice.Tweedle.Interop;
+using Alice.Player.Unity;
+using BeauRoutine;
 
 public class UserInput : MonoBehaviour
 {
@@ -10,12 +13,52 @@ public class UserInput : MonoBehaviour
     public InputField inputField;
     public Button doneButton;
 
+    private Routine m_Routine;
+    private bool userInputDone = false;
+
+    void Awake()
+    {
+        doneButton.onClick.AddListener(()=>{
+            UserClickedButton();
+        });
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            UserClickedButton();
+        }
+    }
+
+    public void Spawn(string label, AsyncReturn<string> stringReturn)
+    {
+        SetLabel(label);
+        userInputDone = false;
+        m_Routine.Replace(this, WaitForUserToPopulate(stringReturn));
+    }
+
     public void SetLabel(string inputLabel)
     {
         label.SetText(inputLabel);
     }
+
     public string GetUserInput()
     {
+        return inputField.text;
+    }
 
+    public void UserClickedButton()
+    {
+        userInputDone = true;
+    }
+
+    private IEnumerator WaitForUserToPopulate(AsyncReturn<string> returnString)
+    {
+        while(!userInputDone){
+            yield return null;
+        }
+        returnString.Return(GetUserInput());
+        Destroy(this.gameObject);
     }
 }
