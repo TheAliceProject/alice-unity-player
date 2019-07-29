298e864eb51d0f60c38542c68da939c8577eb281
rfrankfilament <rfrank@filamentgames.com>
2019-07-29 13:40:40 -0500

    User input init

---
+++ Assets/_Scripts/Player/Modules/SceneGraph/UI/UserInput.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UserInput : MonoBehaviour
+{
+    public TextMeshProUGUI label;
+    public InputField inputField;
+    public Button doneButton;
+
+    public void SetLabel(string inputLabel)
+    {
+        label.SetText(inputLabel);
+    }
+    public string GetUserInput()
+    {
+
+    }
+}
