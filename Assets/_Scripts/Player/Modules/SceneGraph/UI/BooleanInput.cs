f95aaabe806d983e44cf387e483c01eb31376846
rfrankfilament <rfrank@filamentgames.com>
2019-07-29 15:40:24 -0500

    Boolean input

---
+++ Assets/_Scripts/Player/Modules/SceneGraph/UI/BooleanInput.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Alice.Tweedle.Interop;
+using Alice.Player.Unity;
+using BeauRoutine;
+
+public class BooleanInput : MonoBehaviour
+{
+    public TextMeshProUGUI label;
+    public Button trueButton;
+    public Button falseButton;
+
+    private Routine m_Routine;
+    private BoolState boolState = BoolState.None;
+    private enum BoolState
+    {
+        None,
+        True,
+        False
+    }
+    void Awake()
+    {
+        trueButton.onClick.AddListener(()=>{
+            UserClickedButton(true);
+        });
+        falseButton.onClick.AddListener(()=>{
+            UserClickedButton(false);
+        });
+    }
+
+    public void Spawn(string label, AsyncReturn<bool> boolReturn)
+    {
+        SetLabel(label);
+        boolState = BoolState.None;
+        m_Routine.Replace(this, WaitForUserToPopulate(boolReturn));
+    }
+
+    public void SetLabel(string inputLabel)
+    {
+        label.SetText(inputLabel);
+    }
+
+    public void UserClickedButton(bool clickedTrue)
+    {
+        boolState = clickedTrue ? BoolState.True : BoolState.False;
+    }
+
+    private IEnumerator WaitForUserToPopulate(AsyncReturn<bool> returnBool)
+    {
+        while(boolState == BoolState.None){
+            yield return null;
+        }
+        returnBool.Return(boolState == BoolState.True);
+        Destroy(this.gameObject);
+    }
+}
