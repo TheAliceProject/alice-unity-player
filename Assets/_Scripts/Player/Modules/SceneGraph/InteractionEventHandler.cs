b84ddb2c085e5305d8bf9bdc5a5b15120a8ddfcc
Rob <robertfrank@uwalumni.com>
2019-08-19 16:35:49 -0500

    Proximity events

--- Assets/_Scripts/Player/Modules/SceneGraph/InteractionEventHandler.cs
+++ Assets/_Scripts/Player/Modules/SceneGraph/InteractionEventHandler.cs
@@ -10,6 +10,20 @@ namespace Alice.Player.Unity
     {
         private List<CollisionEventListenerProxy> m_CollisionListeners = new List<CollisionEventListenerProxy>();
         private List<ViewEventListenerProxy> m_ViewListeners = new List<ViewEventListenerProxy>();
+        private List<ProximityEventListenerProxy> m_ProximityListeners = new List<ProximityEventListenerProxy>();
+
+        // Called from Update()
+        public void HandleInteractionEvents()
+        {
+            // Check proximity listeners
+            if(m_ProximityListeners.Count > 0)
+            {
+                for (int i = 0; i < m_ProximityListeners.Count; i++)
+                {
+                    m_ProximityListeners[i].CheckDistances();
+                }
+            }
+        }
 
         public void AddCollisionListener(CollisionEventListenerProxy listener)
         {
@@ -19,7 +33,11 @@ namespace Alice.Player.Unity
         {
             m_ViewListeners.Add(listener);
         }
-
+        public void AddProximityListener(ProximityEventListenerProxy listener)
+        {
+            m_ProximityListeners.Add(listener);
+        }
+        
         public void NotifyObjectsCollided(SGEntity object1, SGEntity object2, bool enter)
         {
             for(int i = 0; i < m_CollisionListeners.Count; i++)
