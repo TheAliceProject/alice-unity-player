359f5ea1ce6d244be5d7f6325f098d0a9d3487ca
Rob <robertfrank@uwalumni.com>
2019-08-26 17:10:35 -0500

    Finish up global ui

---
+++ Assets/_Scripts/Player/WorldControl.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class WorldControl : MonoBehaviour
+{
+    public Button reloadButton;
+    public GameObject introCanvas;
+    public Camera CameraPrefab;
+    public UISlidedown uISlidedown;
+
+    public Button speedUpButton;
+    public Button slowDownButton;
+    public Button pauseButton;
+    public TextMeshProUGUI status;
+
+    private float currentTimeScale = 1f;
+    private bool paused = false;
+
+    void Start()
+    {
+        reloadButton.onClick.AddListener(() =>
+        {
+            Destroy(GameObject.Find("SceneGraph"));
+            introCanvas.gameObject.SetActive(true);
+            Camera newCamera = Instantiate(CameraPrefab);
+            newCamera.tag = "MainCamera";
+            Time.timeScale = currentTimeScale = 1f;
+            uISlidedown.ForceSlide(true);
+        });
+
+        speedUpButton.onClick.AddListener(() =>
+        {
+            currentTimeScale *= 2f;
+            Time.timeScale = currentTimeScale;
+            UpdateStatus();
+        });
+
+        slowDownButton.onClick.AddListener(() =>
+        {
+            currentTimeScale /= 2f;
+            Time.timeScale = currentTimeScale;
+            UpdateStatus();
+        });
+
+        pauseButton.onClick.AddListener(() =>
+        {
+            if(paused){
+                Time.timeScale = currentTimeScale;
+            }
+            else{
+                Time.timeScale = 0f;
+            }
+            paused = !paused;
+            UpdateStatus();
+        });
+
+    }
+
+    void UpdateStatus()
+    {
+        status.text = string.Format("Speed: {0:0.0}x", Time.timeScale);
+    }
+}
