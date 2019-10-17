a4a17d850f9732c918f5c293fc507d6a80ca462e
Rob <robertfrank@uwalumni.com>
2019-10-17 10:53:32 -0500

    Capture VR button presses

--- Assets/_Scripts/Player/VRControl.cs
+++ Assets/_Scripts/Player/VRControl.cs
@@ -18,6 +18,8 @@ public class VRControl : MonoBehaviour
     public static VRControl I { get { return _instance; } }
     private Routine m_routine;
 
+    private const float TRIGGER_SENSITIVITY = 0.95f;
+
     // Start is called before the first frame update
     void Awake()
     {
@@ -46,6 +48,55 @@ public class VRControl : MonoBehaviour
             }
         }
 #endif
+    }
+
+    void Update()
+    {
+        if(Input.GetAxis("RightTrigger") > TRIGGER_SENSITIVITY)
+            UnityEngine.Debug.Log("Right trigger pressed!");
+        else if (Input.GetAxis("LeftTrigger") > TRIGGER_SENSITIVITY)
+            UnityEngine.Debug.Log("Left trigger pressed!");
+        else if (Input.GetButtonDown("RightGrip"))
+            UnityEngine.Debug.Log("Right grip pressed!");
+        else if (Input.GetButtonDown("LeftGrip"))
+            UnityEngine.Debug.Log("Left grip pressed!");
+
+        else if (Input.GetButtonDown("PrimaryRight"))
+            UnityEngine.Debug.Log("Right primary pressed!");
+        else if (Input.GetButtonDown("PrimaryLeft"))
+            UnityEngine.Debug.Log("Left primary pressed!");
+        else if (Input.GetButtonDown("SecondaryRight"))
+            UnityEngine.Debug.Log("Right secondary pressed!");
+        else if (Input.GetButtonDown("SecondaryLeft"))
+            UnityEngine.Debug.Log("Left secondary pressed!");
+        else if (Input.GetButtonDown("MenuRight"))
+            UnityEngine.Debug.Log("Right menu pressed!");
+        else if (Input.GetButtonDown("MenuLeft"))
+            UnityEngine.Debug.Log("Left menu pressed!");
+
+        else if (Input.GetAxis("RightThumbstickLeftRight") > TRIGGER_SENSITIVITY)
+            UnityEngine.Debug.Log("Right thumbstick right!");
+        else if (Input.GetAxis("RightThumbstickLeftRight") < -TRIGGER_SENSITIVITY)
+            UnityEngine.Debug.Log("Right thumbstick left!");
+        else if (Input.GetAxis("RightThumbstickUpDown") > TRIGGER_SENSITIVITY)
+            UnityEngine.Debug.Log("Right thumbstick up!");
+        else if (Input.GetAxis("RightThumbstickUpDown") < -TRIGGER_SENSITIVITY)
+            UnityEngine.Debug.Log("Right thumbstick down!");
+        else if(Input.GetButtonDown("RightThumbstickClick"))
+            UnityEngine.Debug.Log("Right thumbstick clicked");
+
+        else if (Input.GetAxis("LeftThumbstickLeftRight") > TRIGGER_SENSITIVITY)
+            UnityEngine.Debug.Log("Left thumbstick right!");
+        else if (Input.GetAxis("LeftThumbstickLeftRight") < -TRIGGER_SENSITIVITY)
+            UnityEngine.Debug.Log("Left thumbstick left!");
+        else if (Input.GetAxis("LeftThumbstickUpDown") > TRIGGER_SENSITIVITY)
+            UnityEngine.Debug.Log("Left thumbstick up!");
+        else if (Input.GetAxis("LeftThumbstickUpDown") < -TRIGGER_SENSITIVITY)
+            UnityEngine.Debug.Log("Left thumbstick down!");
+        else if (Input.GetButtonDown("LeftThumbstickClick"))
+            UnityEngine.Debug.Log("Left thumbstick clicked");
+
+
     }
 
     public void SetVROutput(string deviceToLoad)
