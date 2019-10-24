e6569974ac3cb2a37432b4e95c13804239e0863a
Rob <robertfrank@uwalumni.com>
2019-10-24 11:59:08 -0500

    Fallthrough keyboard keys for VR
    Move Trigger press checks to VRControl

--- Assets/VRRig.cs
+++ Assets/VRRig.cs
@@ -15,36 +15,49 @@ public class VRRig : MonoBehaviour
 
     void Awake()
     {
-        rightRend = rightController.gameObject.AddComponent<LineRenderer>();
-        rightRend.material = new Material(Shader.Find("Sprites/Default"));
-        rightRend.startColor = Color.white;
-        rightRend.endColor = Color.clear;
-        rightRend.startWidth = 0.01f;
-        rightRend.positionCount = 2;
-
-        leftRend = leftController.gameObject.AddComponent<LineRenderer>();
-        leftRend.material = new Material(Shader.Find("Sprites/Default"));
-        leftRend.startColor = Color.white;
-        leftRend.endColor = Color.clear;
-        leftRend.startWidth = 0.01f;
-        leftRend.positionCount = 2;
-
+        CreatePointers();
         leftRend.enabled = false;
         rightRend.enabled = false;
         laserPointersEnabled = false;
     }
 
+    private void CreatePointers()
+    {
+        if(rightController != null){
+            rightRend = rightController.gameObject.AddComponent<LineRenderer>();
+            rightRend.material = new Material(Shader.Find("Sprites/Default"));
+            rightRend.startColor = Color.white;
+            rightRend.endColor = Color.clear;
+            rightRend.startWidth = 0.01f;
+            rightRend.positionCount = 2;
+        }
+        if(leftController != null){
+            leftRend = leftController.gameObject.AddComponent<LineRenderer>();
+            leftRend.material = new Material(Shader.Find("Sprites/Default"));
+            leftRend.startColor = Color.white;
+            leftRend.endColor = Color.clear;
+            leftRend.startWidth = 0.01f;
+            leftRend.positionCount = 2;
+        }
+    }
+
     public void EnableLaserPointers(bool ena)
     {
-        leftRend.enabled = ena;
-        rightRend.enabled = ena;
+        if(leftRend != null)
+            leftRend.enabled = ena;
+        if(rightRend != null)
+            rightRend.enabled = ena;
         laserPointersEnabled = ena;
     }
 
     void Update()
     {
         if(laserPointersEnabled)
-        {
+        {   
+            if(this == null || rightRend == null || leftRend == null){
+                laserPointersEnabled = false;
+                return;
+            }
             rightRend.SetPosition(0, rightController.position);
             rightRend.SetPosition(1, rightController.position + (rightController.forward * 10f));
 
