af45af27c991c6f191b7edd605bafe5a3fee3668
Rob <robertfrank@uwalumni.com>
2019-11-05 17:06:42 -0600

    Wip world control

--- Assets/_Scripts/Player/UISlidedown.cs
+++ Assets/_Scripts/Player/UISlidedown.cs
@@ -13,30 +13,52 @@ public class UISlidedown : MonoBehaviour, IPointerEnterHandler
 
     private Routine m_routine;
 
-    private const float downYPosition = 30f;
-    private const float upYPosition = 100f;
-    private const float moveTime = 0.25f;
+    private const float onYPosition = 30f;
+    private const float offYPosition = 100f;
 
+    private const float onYPositionVR = 250f;
+    private const float offYPositionVR = -100f;
+
+    private const float moveTime = 0.25f;
+    private bool controlsActive = false;
+    
     void IPointerEnterHandler.OnPointerEnter(PointerEventData eventData)
     {
-        GameObject sceneGraphTest = GameObject.Find("SceneGraph");
-        if(sceneGraphTest == null)
-            return;
-        m_routine.Replace(this, CheckHoveringOverUI());
+        if(!VRControl.IsLoadedInVR())
+        {
+            GameObject sceneGraphTest = GameObject.Find("SceneGraph");
+            if (sceneGraphTest == null)
+                return;
+            m_routine.Replace(this, CheckHoveringOverUI());
+        }
     }
     
     IEnumerator CheckHoveringOverUI()
     {
-        yield return anchor.AnchorPosTo(downYPosition, moveTime * Time.timeScale, Axis.Y).Ease(Curve.BackOut);
+        yield return anchor.AnchorPosTo(GetOnPosition(), moveTime * Time.timeScale, Axis.Y).Ease(Curve.BackOut);
         while(EventSystem.current.IsPointerOverGameObject())
         {
             yield return null;
         }
-        yield return anchor.AnchorPosTo(upYPosition, moveTime * Time.timeScale, Axis.Y).Ease(Curve.BackOut);
+        yield return anchor.AnchorPosTo(GetOffPosition(), moveTime * Time.timeScale, Axis.Y).Ease(Curve.BackOut);
     }
 
-    public void ForceSlide(bool up)
+    public void ForceSlide(bool on)
     {
-        m_routine.Replace(this, anchor.AnchorPosTo(up ? upYPosition : downYPosition, moveTime * Time.timeScale, Axis.Y).Ease(Curve.BackOut));
+        controlsActive = on;
+        m_routine.Replace(this, anchor.AnchorPosTo(on ? GetOffPosition() : GetOnPosition(), moveTime * Time.timeScale, Axis.Y).Ease(Curve.BackOut));
+    }
+
+    public void ToggleSlide()
+    {
+        ForceSlide(!controlsActive);
+    }
+
+    private float GetOnPosition(){
+        return VRControl.IsLoadedInVR() ? onYPositionVR : onYPosition;
+    }
+
+    private float GetOffPosition(){
+        return VRControl.IsLoadedInVR() ? offYPositionVR : offYPosition;
     }
 }
