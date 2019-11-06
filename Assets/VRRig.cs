8103d58b1224ef61ea07f968870dd2abebd65c61
Rob <robertfrank@uwalumni.com>
2019-11-06 10:53:29 -0600

    Bring world control to VR

--- Assets/VRRig.cs
+++ Assets/VRRig.cs
@@ -16,6 +16,7 @@ public class VRRig : MonoBehaviour
     private EventSystem eventSystem;
     private bool enabledForUI = false;
     private bool enabledForManipulation = false;
+    private bool enabledForControl = false;
 
     private List<GameObject> selectedButtons = new List<GameObject>();
 
@@ -24,6 +25,7 @@ public class VRRig : MonoBehaviour
         rightRend.enabled = false;
         enabledForManipulation = false;
         enabledForUI = false;
+        enabledForControl = false;
         eventSystem = VRControl.EventSystem();
     }
 
@@ -39,8 +41,13 @@ public class VRRig : MonoBehaviour
     }
 
     public void EnablePointersForUI(bool ena){
-        EnablePointers(ena || enabledForManipulation);
+        EnablePointers(ena || enabledForManipulation || enabledForControl);
         enabledForUI = ena;
+    }
+
+    public void EnablePointersForControl(bool ena){
+        EnablePointers(ena || enabledForManipulation || enabledForUI);
+        enabledForControl = ena;
     }
     
     public void EnablePointersForManipulation(bool ena)
@@ -50,10 +57,10 @@ public class VRRig : MonoBehaviour
     }
 
     void Update(){
-        if(enabledForManipulation || enabledForUI){
+        if(enabledForManipulation || enabledForUI || enabledForControl){
             float leftPointerDistance = 10f;
             float rightPointerDistance = 10f;
-            if(enabledForUI){
+            if(enabledForUI || enabledForControl){
                 leftPointerDistance = CheckHandRaycasts(leftController);
                 rightPointerDistance = CheckHandRaycasts(rightController);
             }
@@ -80,15 +87,17 @@ public class VRRig : MonoBehaviour
                     ExecuteEvents.Execute(hitButton.gameObject, new BaseEventData(eventSystem), ExecuteEvents.submitHandler);
                 }
             }
-            else if (selectedButtons.Count > 0){
-                for (int i = selectedButtons.Count - 1; i >= 0; i--)
-                {
-                    ExecuteEvents.Execute(selectedButtons[i], new BaseEventData(eventSystem), ExecuteEvents.deselectHandler);
-                    selectedButtons.RemoveAt(i);
-                }
-                    
-            }
         }
+        if (selectedButtons.Count > 0)
+        {
+            for (int i = selectedButtons.Count - 1; i >= 0; i--)
+            {
+                ExecuteEvents.Execute(selectedButtons[i], new BaseEventData(eventSystem), ExecuteEvents.deselectHandler);
+                selectedButtons.RemoveAt(i);
+            }
+
+        }
+
         return pointerDistance;
     }
 }
