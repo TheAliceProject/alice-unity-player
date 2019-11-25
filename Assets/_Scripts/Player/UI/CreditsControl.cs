875488c4cf013b2b1ff04eef177b134bbe10291d
Rob <robertfrank@uwalumni.com>
2019-11-25 13:23:03 -0600

    Credits UI, World loader control UI, and handle no recent worlds better

---
+++ Assets/_Scripts/Player/UI/CreditsControl.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Alice.Tweedle.Parse;
+
+public class CreditsControl : MonoBehaviour
+{
+    public TextMeshProUGUI versionString;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (versionString)
+            versionString.text = string.Format("Player Ver {0}\nLibrary Ver {1}", PlayerLibraryManifest.Instance.PlayerLibraryVersion, PlayerLibraryManifest.Instance.GetLibraryVersion());
+    }
+}
