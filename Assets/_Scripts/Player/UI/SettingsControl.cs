ec7d01d62093b09102f2841a134041ab027d58a0
rfrankfilament <rfrank@filamentgames.com>
2019-12-03 16:50:31 -0600

    Save settings to PlayerPrefs

--- Assets/_Scripts/Player/UI/SettingsControl.cs
+++ Assets/_Scripts/Player/UI/SettingsControl.cs
@@ -3,12 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using System.IO;
 
 public class SettingsControl : MonoBehaviour
 {
+    private class Settings{
+        public int width;
+        public int height;
+        public int fullScreen;
+    }
+
     public TMP_Dropdown dropdown;
     public Button applyButton;
     public Toggle fullScreen;
+    private Settings settings = new Settings();
     private bool dirty;
     private string[] supportedResolutions = { "1024 x 768", 
                                             "1280 x 720", 
@@ -19,6 +27,8 @@ public class SettingsControl : MonoBehaviour
                                             "1920 x 1080", 
                                             "1920 x 1200", 
                                             "2560 x 1440"};
+    private const string settingsFile = "settings.txt";
+
     // Start is called before the first frame update
     void Start()
     {
@@ -49,12 +59,43 @@ public class SettingsControl : MonoBehaviour
 
         dropdown.ClearOptions();
         dropdown.AddOptions(options);
+        ReadSettings();
+        this.enabled = false;
+    }
+
+    private void SaveSettings()
+    {
+        PlayerPrefs.SetInt("ScreenWidth", settings.width);
+        PlayerPrefs.SetInt("ScreenHeight", settings.height);
+        PlayerPrefs.SetInt("Fullscreen", settings.fullScreen > 0 ? 1 : 0);
+    }
+    private void ReadSettings()
+    {
+        settings.fullScreen = PlayerPrefs.GetInt("Fullscreen", 0);
+        settings.width = PlayerPrefs.GetInt("ScreenWidth", 1024);
+        settings.height = PlayerPrefs.GetInt("ScreenHeight", 768);
+
+        fullScreen.isOn = settings.fullScreen > 0;
+        string width = settings.width.ToString();
+        string height = settings.height.ToString();
+        for(int i = 0; i < dropdown.options.Count; i++){
+            if(dropdown.options[i].text == width + " x " + height){
+                dropdown.value = i;
+                break;
+            }
+        }
+        Screen.SetResolution(settings.width, settings.height, fullScreen.isOn);
     }
 
     private void ApplySettings()
     {
         string[] parsed = ParseResolution(dropdown.options[dropdown.value].text);
-        Screen.SetResolution(int.Parse(parsed[0]), int.Parse(parsed[1]), fullScreen.isOn);
+        settings.width = int.Parse(parsed[0]);
+        settings.height = int.Parse(parsed[1]);
+        settings.fullScreen = fullScreen.isOn ? 1 : 0;
+
+        Screen.SetResolution(settings.width, settings.height, fullScreen.isOn);
+        SaveSettings();
     }
 
     private string[] ParseResolution(string resolutionString)
