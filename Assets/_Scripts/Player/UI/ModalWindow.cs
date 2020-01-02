b29b22c054464571ec84c0a3541ee2e800843418
Rob <robertfrank@uwalumni.com>
2020-01-02 13:17:37 -0600

    Scale modal window and pop out

--- Assets/_Scripts/Player/UI/ModalWindow.cs
+++ Assets/_Scripts/Player/UI/ModalWindow.cs
@@ -3,22 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using BeauRoutine;
 
 public class ModalWindow : MonoBehaviour
 {
-    public TextMeshProUGUI headerText;
+    public RectTransform dialog;
     public TextMeshProUGUI bodyText;
     public Button okButton;
-    
+
+    private Routine m_routine;
     // Start is called before the first frame update
     void Start()
     {
-        okButton.onClick.AddListener(() => { Destroy(this.gameObject); });
+        okButton.onClick.AddListener(() => { m_routine.Replace(this, PopRoutine(false)); });
+        dialog.SetScale(0f, Axis.XY);
+        m_routine.Replace(this, PopRoutine(true));
     }
 
     public void SetData(string header, string body)
     {
-        headerText.SetText(header);
-        bodyText.SetText(body);
+        string modalText = string.Format("<size=100%>{0}\n\n<size=75%>{1}", header, body);
+        bodyText.SetText(modalText);
+    }
+
+    IEnumerator PopRoutine(bool popIn)
+    {
+        if(popIn)
+        {
+            yield return dialog.ScaleTo(1f, 0.4f, Axis.XY).Ease(Curve.CubeInOut);
+        }
+        else{
+            yield return dialog.ScaleTo(0f, 0.4f, Axis.XY).Ease(Curve.CubeInOut);
+            Destroy(this.gameObject);
+        }
     }
 }
