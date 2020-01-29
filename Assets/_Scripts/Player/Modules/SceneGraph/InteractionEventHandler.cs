03ba70dcca411b0f3cb3dc5cf8c45e0578946f3c
Daniel Abeshouse <dabeshouse@gmail.com>
2020-01-29 13:05:15 -0500

    T804 Ensure collision listeners get arguments in expected order. Consolidate collision tracking on handler.

--- Assets/_Scripts/Player/Modules/SceneGraph/InteractionEventHandler.cs
+++ Assets/_Scripts/Player/Modules/SceneGraph/InteractionEventHandler.cs
@@ -1,39 +1,37 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using Alice.Player.Modules;
-using Alice.Player.Primitives;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Alice.Player.Unity 
 {
     public class InteractionEventHandler
     {
-        private List<CollisionEventListenerProxy> m_CollisionListeners = new List<CollisionEventListenerProxy>();
-        private List<ViewEventListenerProxy> m_ViewListeners = new List<ViewEventListenerProxy>();
-        private List<ProximityEventListenerProxy> m_ProximityListeners = new List<ProximityEventListenerProxy>();
-        private List<PointOfViewChangeEventListenerProxy> m_PovListeners = new List<PointOfViewChangeEventListenerProxy>();
-        private List<OcclusionEventListenerProxy> m_OcclusionListeners = new List<OcclusionEventListenerProxy>();
+        private readonly List<CollisionEventListenerProxy> m_CollisionListeners = new List<CollisionEventListenerProxy>();
+        private readonly List<ViewEventListenerProxy> m_ViewListeners = new List<ViewEventListenerProxy>();
+        private readonly List<ProximityEventListenerProxy> m_ProximityListeners = new List<ProximityEventListenerProxy>();
+        private readonly List<PointOfViewChangeEventListenerProxy> m_PovListeners = new List<PointOfViewChangeEventListenerProxy>();
+        private readonly List<OcclusionEventListenerProxy> m_OcclusionListeners = new List<OcclusionEventListenerProxy>();
+        
+        
+        private readonly List<OverlappingPair> m_Collisions = new List<OverlappingPair>();
 
         // Called from Update()
         public void HandleInteractionEvents()
         {
             // Check proximity listeners
-            if(m_ProximityListeners.Count > 0){
-                for (int i = 0; i < m_ProximityListeners.Count; i++){
-                    m_ProximityListeners[i].CheckDistances();
-                }
+            foreach (var listener in m_ProximityListeners)
+            {
+                listener.CheckDistances();
             }
 
             // Check POV change listeners
-            if(m_PovListeners.Count > 0){
-                for (int i = 0; i < m_PovListeners.Count; i++){
-                    m_PovListeners[i].CheckChanges();
-                }
+            foreach (var listener in m_PovListeners)
+            {
+                listener.CheckChanges();
             }
 
-            for (int i = 0; i < m_OcclusionListeners.Count; i++)
+            foreach (var listener in m_OcclusionListeners)
             {
-                m_OcclusionListeners[i].UpdateOcclusions();
+                listener.UpdateOcclusions();
             }
         }
 
@@ -60,25 +58,39 @@ namespace Alice.Player.Unity
         
         public void NotifyObjectsCollided(SGEntity object1, SGEntity object2, bool enter)
         {
-            for(int i = 0; i < m_CollisionListeners.Count; i++)
+            var pair = m_Collisions.FirstOrDefault(p => p.ContainsBoth(object1, object2));
+            if (pair != null)
             {
-                m_CollisionListeners[i].NotifyEvent(object1,  object2, enter);
+                pair.UpdateOverlaps(enter);
+            }
+            else
+            {
+                pair = new OverlappingPair(object1, object2);
+                m_Collisions.Add(pair);
+            }
+
+            if(!pair.IsEnter() && !pair.IsExit())
+                return;
+
+            foreach (var listener in m_CollisionListeners)
+            {
+                listener.NotifyEvent(object1,  object2, enter);
             }
         }
 
         public void NotifyObjectsOccluded(SGModel foregroundObject, SGModel backgroundObject)
         {
-            for (int i = 0; i < m_OcclusionListeners.Count; i++)
+            foreach (var listener in m_OcclusionListeners)
             {
-                m_OcclusionListeners[i].NotifyEvent(foregroundObject, backgroundObject);
+                listener.NotifyEvent(foregroundObject, backgroundObject);
             }
         }
 
         public void NotifyModelInView(SGModel model, bool enteredView)
         {
-            for (int i = 0; i < m_ViewListeners.Count; i++)
+            foreach (var listener in m_ViewListeners)
             {
-                m_ViewListeners[i].NotifyEvent(model, enteredView);
+                listener.NotifyEvent(model, enteredView);
             }
         }
     }
