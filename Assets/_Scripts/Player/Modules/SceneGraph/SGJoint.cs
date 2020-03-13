c6cc61c24ab49a7f6b7735ff306a0310384709a9
Daniel Abeshouse <dabeshouse@gmail.com>
2020-03-13 10:58:14 -0400

    Remove modifiers from joint construction

--- Assets/_Scripts/Player/Modules/SceneGraph/SGJoint.cs
+++ Assets/_Scripts/Player/Modules/SceneGraph/SGJoint.cs
@@ -1,12 +1,6 @@
-using Alice.Player.Primitives;
-
 namespace Alice.Player.Unity {
     public sealed class SGJoint : SGTransformableEntity {
         private SGJointedModel m_parentModel;
-        // To project from Sims joint space into Alice
-        public VantagePoint modifier = VantagePoint.IDENTITY;
-        // To project from Alice joint space into Sims
-        public VantagePoint modifierInverse = VantagePoint.IDENTITY;
 
         public override void CleanUp(){}
 
@@ -16,23 +10,6 @@ namespace Alice.Player.Unity {
 
         public SGJointedModel GetParentJointedModel(){
             return m_parentModel;
-        }
-
-        protected override void StoreAliceTransformation(VantagePoint vp) {
-            base.StoreAliceTransformation(vp.multiply(modifierInverse));
-        }
-
-        public override VantagePoint GetLocalTransformation() {
-            return VantagePoint.FromUnity(cachedTransform.localPosition, cachedTransform.localRotation, modifier);
-        }
-
-        public override VantagePoint GetAbsoluteTransformation() {
-            return VantagePoint.FromUnity(cachedTransform.position, cachedTransform.rotation, modifier);
-        }
-
-        public void SetReorientation(Orientation orientation) {
-            modifier = new VantagePoint(orientation);
-            modifierInverse = modifier.inverse();
         }
     }
 }
