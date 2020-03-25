0ee3bde11e7706b51c5f9827bb02c348364f0d7b
zrdumped <nmgzhangran@163.com>
2020-03-25 17:02:06 -0400

    Add short circuit for and and or, together with unit tests

--- Assets/_Scripts/Tweedle/Tweedle/Expression/LogicalAndExpression.cs
+++ Assets/_Scripts/Tweedle/Tweedle/Expression/LogicalAndExpression.cs
@@ -1,16 +1,27 @@
-﻿namespace Alice.Tweedle
+﻿using Alice.Tweedle.VM;
+
+namespace Alice.Tweedle
 {
     class LogicalAndExpression : BinaryExpression
     {
+        ITweedleExpression lhs;
+        ITweedleExpression rhs;
 
         public LogicalAndExpression(ITweedleExpression lhs, ITweedleExpression rhs)
             : base(lhs, rhs, TBuiltInTypes.BOOLEAN)
         {
+            this.lhs = lhs;
+            this.rhs = rhs;
         }
 
         protected override TValue Evaluate(TValue left, TValue right)
         {
             return TBuiltInTypes.BOOLEAN.Instantiate(left.ToBoolean() && right.ToBoolean());
+        }
+
+        public override ExecutionStep AsStep(ExecutionScope scope)
+        {
+            return new TwoValueComputationStep(ToTweedle(), scope, lhs, rhs, Evaluate, true, false);
         }
 
         internal override string Operator()
--- Assets/_Scripts/Tweedle/Tweedle/Expression/LogicalOrExpression.cs
+++ Assets/_Scripts/Tweedle/Tweedle/Expression/LogicalOrExpression.cs
@@ -1,16 +1,27 @@
-﻿namespace Alice.Tweedle
+﻿using Alice.Tweedle.VM;
+
+namespace Alice.Tweedle
 {
     class LogicalOrExpression : BinaryExpression
     {
+        ITweedleExpression lhs;
+        ITweedleExpression rhs;
 
         public LogicalOrExpression(ITweedleExpression lhs, ITweedleExpression rhs)
             : base(lhs, rhs, TBuiltInTypes.BOOLEAN)
         {
+            this.lhs = lhs;
+            this.rhs = rhs;
         }
 
         protected override TValue Evaluate(TValue left, TValue right)
         {
             return TBuiltInTypes.BOOLEAN.Instantiate(left.ToBoolean() || right.ToBoolean());
+        }
+
+        public override ExecutionStep AsStep(ExecutionScope scope)
+        {
+            return new TwoValueComputationStep(ToTweedle(), scope, lhs, rhs, Evaluate, false, true);
         }
 
         internal override string Operator()
