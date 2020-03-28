0404a0a097c80fcda90e76c6b29e599aa74ffbff
Daniel Abeshouse <dabeshouse@gmail.com>
2020-03-28 10:28:49 -0400

    Fix minor warnings

--- Assets/_Scripts/Player/VR/VRBrowser.cs
+++ Assets/_Scripts/Player/VR/VRBrowser.cs
@@ -58,7 +58,7 @@ public class VRBrowser : MonoBehaviour
                 {
                     System.IO.Directory.GetDirectories(pwd + path);
                 }
-                catch (Exception e)
+                catch (Exception)
                 {
                     return;
                 }
