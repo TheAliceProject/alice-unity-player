9e38fbab9c65d69e5085342e41fda65761de6e26
Daniel Abeshouse <dabeshouse@gmail.com>
2023-07-24 15:12:04 -0400

    Add SGVRHeadset and device. SGCamera acts SGVRUser

--- Assets/_Scripts/Player/Modules/SceneGraph/SGVRHand.cs
+++ Assets/_Scripts/Player/Modules/SceneGraph/SGVRHand.cs
@@ -1,24 +1,9 @@
-using Alice.Player.Primitives;
-
 namespace Alice.Player.Unity {
-    public sealed class SGVRHand : SGTransformableEntity {
-        private SGCamera m_camera;
-
-        public override void CleanUp(){}
-
-        public void SetCamera(SGCamera camera){
-            m_camera = camera;
-            vehicle = camera;
-            transform.SetParent(camera.GetHandFor(gameObject.name));
+    public sealed class SGVRHand : SGVRDevice {
+        public override void SetCamera(SGCamera cam){
+            base.SetCamera(cam);
+            transform.SetParent(cam.GetHandFor(gameObject.name));
             transform.localPosition = UnityEngine.Vector3.zero;
-        }
-
-        public SGCamera GetCamera(){
-            return m_camera;
-        }
-
-        public override VantagePoint GetLocalTransformation() {
-            return GetLocalTransformationUpTo(m_camera);
         }
     }
 }
