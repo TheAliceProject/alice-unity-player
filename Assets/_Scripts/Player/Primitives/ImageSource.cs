aa7f98a21770a139b2cd82a8ab075c7dcf84de1c
Daniel Abeshouse <dabeshouse@gmail.con>
2024-02-01 14:28:57 -0500

    Have ImageSource creation return null instead of errors.

--- Assets/_Scripts/Player/Primitives/ImageSource.cs
+++ Assets/_Scripts/Player/Primitives/ImageSource.cs
@@ -18,7 +18,7 @@ namespace Alice.Player.Primitives
         [PInteropConstructor]
         public ImageSource(string resource)
         {
-            Value = WorldObjects.GetGameController().GetTweedleSystem().TextureNamed(resource);
+            Value = WorldObjects.GetGameController()?.GetTweedleSystem()?.TextureNamed(resource);
         }
 
         [PInteropField]
