a716f81a74ae8c18b2bb79260ce4a2af572d7557
keshabagadia <knbagadi@andrew.cmu.edu>
2024-06-17 13:28:35 -0400

    Fixed emoji displaying as unicode numbers (#512) and quote character showing with backslashes in speech bubbles.(#591)

--- Assets/_Scripts/Player/Modules/SceneGraph/UI/SayThinkBubble.cs
+++ Assets/_Scripts/Player/Modules/SceneGraph/UI/SayThinkBubble.cs
@@ -54,6 +54,7 @@ namespace Alice.Player.Unity {
         }
 
         public void SetText(string text, UnityEngine.Color c, TMP_FontAsset font, float scale){
+            text = System.Text.RegularExpressions.Regex.Unescape(text);// Converts escape sequences in strings into the characters they represent
             bubbleText.text = text;
             bubbleText.color = c;
             bubbleText.font = font;
